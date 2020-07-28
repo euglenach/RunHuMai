a9789c6f9f74d64fcf56b133e3013e4e87fa2d3c
Euglenach <midorimushiiii@gmail.com>
2020-07-28 21:31:41 +0900

    🍙

--- RunHuMai/Assets/Scripts/Obstacles/Onigiri.cs
+++ RunHuMai/Assets/Scripts/Obstacles/Onigiri.cs
@@ -1,7 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace Obstacles{
     public class Onigiri : Obstacle{
-        
+        [SerializeField] private float speed;
+        private Rigidbody2D rb;
+        private void Start(){
+            rb = GetComponent<Rigidbody2D>();
+            
+            // rb.velocity = new Vector2(-speed,0);
+        }
+
+        private void Update(){
+            rb.velocity = new Vector2(-speed,rb.velocity.y);
+        }
     }
 }
